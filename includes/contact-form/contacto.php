8cbe9fff36907f1b1cb48feb21b79093e955342a
SMauhourat <santiagomauhourat@hotmail.com>
2021-08-24 15:51:13 -0300

    fix

---
+++ includes/contact-form/contacto.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @version 1.0
+ */
+
+require("class.phpmailer.php");
+require("class.smtp.php");
+
+// Valores enviados desde el formulario
+if ( !isset($_POST["name"]) || !isset($_POST["email"]) || !isset($_POST["message"]) ) {
+    die ("Es necesario completar todos los datos del formulario");
+}
+$nombre = $_POST["name"];
+$email = $_POST["email"];
+$mensaje = $_POST["message"];
+
+// Datos de la cuenta de correo utilizada para enviar vía SMTP
+$smtpHost = "c2330528.ferozo.com";  // Dominio alternativo brindado en el email de alta 
+$smtpUsuario = "web@g2r.com.ar";  // Mi cuenta de correo
+$smtpClave = "G3rman73/";  // Mi contraseña
+
+// Email donde se enviaran los datos cargados en el formulario de contacto
+$emailDestino = "test@g2r.com.ar";
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 465; 
+$mail->SMTPSecure = 'ssl';
+$mail->IsHTML(true); 
+$mail->CharSet = "utf-8";
+
+
+// VALORES A MODIFICAR //
+$mail->Host = $smtpHost; 
+$mail->Username = $smtpUsuario; 
+$mail->Password = $smtpClave;
+
+$mail->From = $email; // Email desde donde envío el correo.
+$mail->FromName = $nombre;
+$mail->AddAddress($emailDestino); // Esta es la dirección a donde enviamos los datos del formulario
+
+$mail->Subject = "G2R - Contacto Web"; // Este es el titulo del email.
+$mensajeHtml = nl2br($mensaje);
+$mail->Body = "{$mensajeHtml} <br /><br />{$nombre}<br />{$email}"; // Texto del email en formato HTML
+$mail->AltBody = "{$mensaje} \n\n {$nombre} \n {$email}"; // Texto sin formato HTML
+// FIN - VALORES A MODIFICAR //
+
+$estadoEnvio = $mail->Send(); 
+if($estadoEnvio){
+    echo "El correo fue enviado correctamente.";
+} else {
+    echo "Ocurrió un error inesperado.";
+}
