361fecc6bfd7671a4654515b5b59361167719056
SMauhourat <santiagomauhourat@hotmail.com>
2021-08-25 09:07:22 -0300

    add instagram

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,7 +162,7 @@ $(function() {
             Circles.create({
                 id: 'circles-2',
                 radius: 50,
-                value: 95,
+                value: 100,
                 maxValue: 100,
                 width: 3,
                 text: function(value){return value + '%';},
@@ -195,24 +195,24 @@ $(function() {
         });
     }
     
-    if($('.our-services-progress').length){
-        $('.our-services-progress').appear(function(){
-            Circles.create({
-                id: 'circles-4',
-                radius: 50,
-                value: 70,
-                maxValue: 100,
-                width: 3,
-                text: function(value){return value + '%';},
-                colors: ['#f0f0f0', '#00C2E3'],
-                duration: 1000,
-                wrpClass: 'circles-wrp',
-                textClass: 'circles-text',
-                styleWrapper: true,
-                styleText: true,
-            });
-        });
-    }
+    // if($('.our-services-progress').length){
+    //     $('.our-services-progress').appear(function(){
+    //         Circles.create({
+    //             id: 'circles-4',
+    //             radius: 50,
+    //             value: 70,
+    //             maxValue: 100,
+    //             width: 3,
+    //             text: function(value){return value + '%';},
+    //             colors: ['#f0f0f0', '#00C2E3'],
+    //             duration: 1000,
+    //             wrpClass: 'circles-wrp',
+    //             textClass: 'circles-text',
+    //             styleWrapper: true,
+    //             styleText: true,
+    //         });
+    //     });
+    // }
    
     
     //===== slick Testimonial
